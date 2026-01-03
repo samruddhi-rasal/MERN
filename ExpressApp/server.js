9ab957bccc6718536f04b987c9e77bbe7e7d8cc0
Samruddhi Rasal <samruddhi.rasal03@gmail.com>
2026-01-03 15:11:39 +0530

    -

--- ExpressApp/server.js
+++ ExpressApp/server.js
@@ -3,8 +3,8 @@ var path=require('path');//imported path module to get relative path
 var app=express();//express is going to return server
                   //get global object from framework                       
                   //global object contains HTTP Server
-//__dirname ----->virtual path-----> http://localhost:8080/
-//          ----------> physical path-----> C:\Users\Samruddhi\OneDrive\Desktop\nodejs\ExpressApp>
+//__dirname ---------->virtual path-------> http://localhost:8080/
+//          ---------->physical path------> C:\Users\Samruddhi\OneDrive\Desktop\nodejs\ExpressApp>
 var staticFolder=express.static(path.join(__dirname,'public'));//to set public as static folder
 app.use(staticFolder);//tell server use it as static folder
 //__dirname is used to get current directory path
@@ -15,8 +15,8 @@ app.use(staticFolder);//tell server use it as static folder
 //handler logic is going to map with app.get
 //mapping incoming request to callback function
 //three request handlers are implemented
-//http://localhost:8080--------callback function
-//http://localhost:8080/hello--------callback function
+//http://localhost:8080----------------callback function
+//http://localhost:8080/hello----------callback function
 //http://localhost:8080/flowers--------callback function
 
 
